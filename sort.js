66a10c53a4f31fa061695d0c81f0f6a2d402f1c1
robertr-dev <73199118+robertr-dev@users.noreply.github.com>
2020-10-21 00:55:06 -0400

    Add files via upload

---
+++ sort.js
@@ -0,0 +1,53 @@
+let arraySize = 40;
+let min = 3;
+let max = 250;
+
+let array = [arraySize];
+let sortedArray = [arraySize];
+
+generateArray();
+
+function generateArray() {
+	for(var i = 0; i < arraySize; i++) {
+		array[i] = generateArrayValue(min, max);
+		
+		if(array[i] == array[i - 1]) {
+			array[i] = generateArrayValue(min, max);
+		}
+	}
+}
+
+function generateArrayValue() {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+console.log("Randomly generated array: ");
+console.log(array);
+
+function bubbleSort() {
+	sortedArray = Array.from(array);
+
+	for(var i = 0; i < arraySize; i++) {
+		for(var j = 0; j < arraySize - 1; j++) {
+			if(sortedArray[j] > sortedArray[j + 1]) {
+				var x = sortedArray[j];
+				sortedArray[j] = sortedArray[j + 1];
+				sortedArray[j + 1] = x;
+			}
+		}
+	}
+}
+
+function insertionSort() {
+	sortedArray = Array.from(array);
+
+	for(var i = 1; i < arraySize; i++) {
+		var current = sortedArray[i];
+		let j = i - 1;
+		while((j > -1) && (current < sortedArray[j])) {
+			sortedArray[j + 1] = sortedArray[j];
+			j--;
+		}
+		sortedArray[j + 1] = current;
+	}
+}
